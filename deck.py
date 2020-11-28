5873b55438a2f9598a76776b87b2a8471ecd103c
atmac0 <ilovemrknets@gmail.com>
2020-11-28 01:15:21 -0500

    refactored hand strength calculation to also return the high card of the player

--- deck.py
+++ deck.py
@@ -90,10 +90,11 @@ def sort_cards(card_list):
         swap_made = False
         for i in range(0, len(card_list)-1):
             if(card_list[i].suit > card_list[i+1].suit):
-                temp_card = card_list[i]
-                card_list[i] = card_list[i+1]
-                card_list[i+1] = temp_card
-                swap_made = True
+                if(card_list[i].rank == card_list[i+1].rank):
+                    temp_card = card_list[i]
+                    card_list[i] = card_list[i+1]
+                    card_list[i+1] = temp_card
+                    swap_made = True
 
         if(swap_made == False):
             suit_sorted = True         
@@ -110,132 +111,258 @@ def sort_cards(card_list):
 # 6: full house
 # 7: four of a kind
 # 8: straight flush
-def get_hand_rank(self, hand, public_cards):
+def get_hand_rank(hand, public_cards):
     all_cards = hand + public_cards
     sorted_cards = sort_cards(all_cards)
+
+    straight_flush, high_card_rank = is_straight_flush(hand, public_cards)
     
-    if(is_straight_flush(sorted_cards)):
-       return 8
-    if(is_four_of_a_kind(sorted_cards)):
-       return 7
-    if(is_full_house(sorted_cards)):
-       return 6
-    if(is_flsuh(sorted_cards)):
-       return 5
-    if(is_straight(sorted_cards)):
-       return 4
-    if(is_three_of_a_kind(sorted_cards)):
-       return 3
-    if(is_two_pair(sorted_cards)):
-       return 2
-    if(is_pair(sorted_cards)):
-       return 1
+    if(straight_flush):
+        return 8, high_card_rank
+
+    four_of_a_kind, high_card_rank = is_four_of_a_kind(sorted_cards)
+   
+    if(four_of_a_kind):
+        return 7, high_card_rank
     
-    return 0
+    full_house, high_card_rank = is_full_house(sorted_cards)
+   
+    if(full_house):
+        return 6, high_card_rank
+
+    flush, high_card_rank = is_flush(hand, public_cards)
+   
+    if(flush):
+        return 5, high_card_rank
+
+    straight, high_card_rank = is_straight(sorted_cards)
+   
+    if(straight):
+        return 4, high_card_rank
+
+    three_of_a_kind, high_card_rank = is_three_of_a_kind(sorted_cards)
+    if(three_of_a_kind):
+        return 3, high_card_rank
+
+    two_pair, high_card_rank = is_two_pair(sorted_cards)
+    if(two_pair):
+        return 2, high_card_rank
+
+    pair, high_card_rank = is_pair(sorted_cards)
+    if(pair):
+        return 1, high_card_rank
+    
+    return 0, get_high_card(hand)
 
 
+def get_high_card(cards):
+    max_rank = cards[0].rank
+    
+    for card in cards:
+        if(card.rank > max_rank):
+            max_rank = card.rank
 
+    return max_rank
 
-def is_straight_flush(sorted_cards):
+def is_straight_flush(hand, public):
+
+    all_cards = hand + public
+    
+    if(len(all_cards) < 5):
+        return False, None
+
+    suit_counter = [[] for i in range(0, NUM_SUITS)]
+    flush_suit = None
+    high_card_rank = None
+
+    # first discover if a flush exists.
+    for card in all_cards:
+        suit_counter[card.suit].append(card)
+        if(len(suit_counter[card.suit]) == 5):
+            flush_suit = card.suit
+
+    if(flush_suit == None):
+        return False, None
+
+    sorted_flush_cards = sort_cards(suit_counter[flush_suit])
+            
     consecutive_counter = 1
-    
-    for i in range(1, len(sorted_cards)):
-        if(sorted_cards[i].rank == sorted_cards[i-1] + 1):
+    consecutive_cards = [sorted_flush_cards[0]]
+    high_card_rank = None
+
+    # then, discover if the flush is also a straight
+    for i in range(1, len(sorted_flush_cards)):
+        if(sorted_flush_cards[i].rank == sorted_flush_cards[i-1].rank + 1):
             consecutive_counter += 1
+            consecutive_cards.append(sorted_flush_cards[i])
+        elif(sorted_flush_cards[i].rank == sorted_flush_cards[i-1].rank):
+            consecutive_cards.append(sorted_flush_cards[i])
         else:
             consecutive_counter = 1
+            consecutive_cards = []
 
-        if(consecutive_counter == 5):
-            if(is_flush(sorted_cards[i-5:i])):
-                return True
+        if(consecutive_counter >= 5):
+            high_card_rank = sorted_flush_cards[i].rank
 
-    return False
+
+    if(high_card_rank != None):
+        return True, high_card_rank
+    
+    return False, None
     
 # returns True if a four of a kind is present. False if not.
 def is_four_of_a_kind(sorted_cards):
-    for i in range(0, len(cards) - 3):
-        if(sorted_cards[i] == sorted_cards[i+1] == sorted_cards[i+2] == sorted_cards[i+3]):
-            return True
+    if(len(sorted_cards) < 4):
+       return False
+    
+    for i in range(0, len(sorted_cards) - 3):
+        if(sorted_cards[i].rank == sorted_cards[i+1].rank == sorted_cards[i+2].rank == sorted_cards[i+3].rank):
+            return True, sorted_cards[i].rank
                 
-    return False
+    return False, None
     
 # determine if a deck is a full house. First, determine if a 3 of a kinds is present. If it is, delete the cards from the list. Then determine if a pair exists.
 def is_full_house(sorted_cards):
-    three_of_a_kind = False
+    if(len(sorted_cards) < 5):
+        return False, None    
     
-    for i in range(0, len(cards) - 2):
-        if(sorted_cards[i] == sorted_cards[i+1] == sorted_cards[i+2]):
+    three_of_a_kind = False
+    three_of_a_kind_rank = None
+    pair_rank = None
+    
+    for i in range(0, len(sorted_cards) - 2):
+        if(sorted_cards[i].rank == sorted_cards[i+1].rank == sorted_cards[i+2].rank):
             three_of_a_kind = True
+            three_of_a_kind_rank = sorted_cards[i].rank
             sorted_cards[i:i+2] = []
+            break
 
-    pair = is_pair(sorted_cards)
+    pair, pair_rank = is_pair(sorted_cards)
 
     if(three_of_a_kind and pair):
-        return True
+        if(three_of_a_kind_rank > pair_rank):
+            return True, three_of_a_kind_rank
+        else:
+            return True, pair_rank
 
-    return False
+    return False, None
 
 
 # returns True if a TOAK is present. False if not.
 def is_three_of_a_kind(sorted_cards):
-    for i in range(0, len(cards) - 2):
-        if(sorted_cards[i] == sorted_cards[i+1] == sorted_cards[i+2]):
-            return True
+    if(len(sorted_cards) < 3):
+        return False, None
+    
+    for i in range(0, len(sorted_cards) - 2):
+        if(sorted_cards[i].rank == sorted_cards[i+1].rank == sorted_cards[i+2].rank):
+            return True, sorted_cards[i].rank
                 
-    return False
+    return False, None
 
-# returns True if a pair is present. False if not.
+# returns True if a pair is present, along with rank of card if a pair is present. False if not. 
 def is_pair(sorted_cards):
+    if(len(sorted_cards) < 2):
+        return False, None
+
+    pair_rank = None
+    
     for i in range(0, len(sorted_cards) - 1):
-        if(sorted_cards[i] == sorted_cards[i+1]):
-            return True
-                
-    return False
+        if(sorted_cards[i].rank == sorted_cards[i+1].rank):
+            if(pair_rank == None):
+                pair_rank = sorted_cards[i].rank
+            elif(sorted_cards[i].rank > pair_rank):
+                pair_rank = sorted_cards[i].rank
+
+    if(pair_rank != None):
+        return True, pair_rank
+    else:
+        return False, pair_rank
 
 # returns True if a pair is present. False if not.
 # determines the rank of the first pair. If a pair of a different rank exists, a two pair exists
 # returns True if a pair is present. False if not.
 def is_two_pair(sorted_cards):
-    rank_of_first_pair = None
+    if(len(sorted_cards) < 4):
+        return False, None
     
-    for i in range(0, len(cards) - 1):
-        if(sorted_cards[i] == sorted_cards[i+1]):
-            if(rank_of_first_pair == None):
-                rank_of_first_pair = sorted_cards[i].rank
-            elif(rank_of_first_pair != sorted_cards[i].rank):
-                return True
-                
-    return False
+    pairs_found = 0
+
+    rank_of_high_pair = None
+    
+    for i in range(0, len(sorted_cards) - 1):
+        if(sorted_cards[i].rank == sorted_cards[i+1].rank):
+            pairs_found += 1
+            if(rank_of_high_pair == None):
+                rank_of_high_pair = sorted_cards[i].rank
+            elif(sorted_cards[i].rank > rank_of_high_pair):
+                rank_of_high_pair = sorted_cards[i].rank
+
+
+    if(pairs_found >= 2):
+        return True, rank_of_high_pair
+    else:
+        return False, rank_of_high_pair
 
 
 # detemine if a hand is a flush
 # count all the cards of each suit. If one of the counts is 5 or above, the hand is a flush
 # returns: None if no flush is present.
 #          An integer corresponding to the suit of the flush if there is a flush
-def is_flush(cards):
-    suit_counter = [0 for i in range(0, NUM_SUITS)]
+def is_flush(hand, public_cards):
 
-    for card in cards:
+    all_cards = hand + public_cards
+    
+    if(len(all_cards) < 5):
+        return False, None
+    
+    suit_counter = [0 for i in range(0, NUM_SUITS)]
+    flush_suit = None
+    high_card_rank = None
+    
+    for card in all_cards:
         suit_counter[card.suit] += 1
         if(suit_counter[card.suit] == 5):
-            return True
-        
-    return False
+            flush_suit = card.suit
+
+    # find the high card of flush in the players hand if a flush exists. This is the high card of the player in a tie scenario
+    if(flush_suit != None):
+        for card in hand:
+            if(card.suit == flush_suit):
+                if(high_card_rank == None):
+                    high_card_rank = card.rank
+                elif(card.rank > high_card_rank):
+                    high_card_rank = card.rank
+
+    if(flush_suit != None):
+        return True, high_card_rank
+    else:
+        return False, high_card_rank
 
 def is_straight(sorted_cards):
+    if(len(sorted_cards) < 5):
+        return False, None
+    
     consecutive_counter = 1
     previous_val = sorted_cards[0].rank
+    high_card_rank = None
+
     
     for card in sorted_cards[1:]:
         if(card.rank == previous_val + 1):
             consecutive_counter += 1
+        elif(card.rank == previous_val):
+            pass
         else:
             consecutive_counter = 1
 
-        if(consecutive_counter == 5):
-            return True
+        # since the card list is sorted, each subsequent card that continues the straight is gauranteed to be of a higher rank
+        if(consecutive_counter >= 5):
+            high_card_card_rank = card.rank
 
         previous_val = card.rank
 
-    return False
+    if(high_card_rank != None):
+        return True, high_card_rank
+    else:
+        return False, high_card_rank
 
--- unit_tests/deck_UT.py
+++ unit_tests/deck_UT.py
@@ -44,11 +44,13 @@ def sort_cards_UT():
 
         if(current_card.rank > next_card.rank):
             print("Failure in sort_cards_UT: current card rank: " + str(current_card.rank) + ", next card rank: " + str(next_card.rank))
+            print("Whole list: ", [(card.suit, card.rank) for card in card_list])
             UT = Test.FAIL
 
         if(current_card.rank == next_card.rank):
             if(current_card.suit > next_card.suit):
                 print("Failure in sort_cards_UT: rank of both: " + str(current_card.rank) + ", current card suit: " + str(current_card.suit) + ", next card suit: " + str(next_card.suit))
+                print("Whole list: ", [(card.suit, card.rank) for card in card_list])
                 UT = Test.FAIL
 
     if(UT == Test.PASS):
@@ -58,55 +60,360 @@ def sort_cards_UT():
     
 
 def get_hand_rank_UT():
-    pass
-
-def is_straight_flush_UT():
-    pass
-
-def is_four_of_a_kind_UT():
-    pass
-
-def is_full_house_UT():
-    pass
-
-def is_three_of_a_kind_UT():
-    pass
-
-def is_pair_UT():
 
     UT = Test.PASS
     
-    pair_cards = [Card(0,0), Card(1,0), Card(2,2), Card(2,12), Card(3,7), Card(2,6), Card(0,3)]
-    no_pair_cards = [Card(0,4), Card(1,0), Card(2,2), Card(2,12), Card(3,7), Card(2,6), Card(0,3)]
+    straight_flush = sort_cards([Card(0,3), Card(0,4), Card(0,5), Card(0,6), Card(0,7), Card(2,12), Card(0,3)])
+    four_of_a_kind = sort_cards([Card(0,3), Card(1,3), Card(2,3), Card(2,12), Card(3,7), Card(2,12), Card(0,3)])
+    full_house = sort_cards([Card(0,0), Card(1,0), Card(2,0), Card(2,12), Card(3,7), Card(2,12), Card(0,3)])
 
-    sorted_pair_cards = sort_cards(pair_cards)
-    sorted_no_pair_cards = sort_cards(no_pair_cards)
+    flush_hand = sort_cards([Card(0,2), Card(3,0)])
+    flush_public = sort_cards([Card(2,0), Card(0,4), Card(0,3),  Card(0,2), Card(0,1)])
+    
+    straight = sort_cards([Card(0,0), Card(1,0), Card(2,4), Card(2,3), Card(3,2), Card(2,2), Card(0,1)])
+    three_of_a_kind = sort_cards([Card(0,3), Card(1,0), Card(2,2), Card(2,3), Card(3,7), Card(2,6), Card(0,3)])
+    two_pair = sort_cards([Card(0,0), Card(1,0), Card(2,2), Card(2,12), Card(3,7), Card(2,7), Card(0,3)])
+    one_pair = sort_cards([Card(0,0), Card(1,0), Card(2,2), Card(2,12), Card(3,7), Card(2,6), Card(0,3)])
+    high_card_list = sort_cards([Card(0,0), Card(1,2), Card(2,4), Card(3,6), Card(3,8), Card(2,10), Card(1,12)])
 
-    if(is_pair(sorted_pair_cards) == False):
+    hand_rank, high_card = get_hand_rank(straight_flush, [])
+    if(hand_rank != 8 and high_card != 7):
+        print("get_hand_rank_UT fail: straight_flush")
         UT = Test.FAIL
-    if(is_pair(sorted_no_pair_cards) == True):
+
+    hand_rank, high_card = get_hand_rank(four_of_a_kind, [])
+    if(hand_rank != 7 and high_card != 3):
+        print("get_hand_rank_UT fail: four_of_a_kind")
         UT = Test.FAIL
 
+    hand_rank, high_card = get_hand_rank(full_house, [])
+    if(hand_rank != 6 and high_card != 0):
+        print("get_hand_rank_UT fail: full_house")
+        UT = Test.FAIL
+
+    hand_rank, high_card = get_hand_rank(flush_hand, flush_public)
+    if(hand_rank != 5 and high_card != 0):
+        print("get_hand_rank_UT fail: flush")
+        UT = Test.FAIL
+
+    hand_rank, high_card = get_hand_rank(straight, [])
+    if(hand_rank != 4 and high_card != 4):
+        print("get_hand_rank_UT fail: straight")
+        UT = Test.FAIL
+
+    hand_rank, high_card = get_hand_rank(three_of_a_kind, [])
+    if(hand_rank != 3 and high_card != 3):
+        print("get_hand_rank_UT fail: three_of_a_kind")
+        UT = Test.FAIL
+
+    hand_rank, high_card = get_hand_rank(two_pair, [])
+    if(hand_rank != 2 and high_card != 0):
+        print("get_hand_rank_UT fail: two_pair")
+        UT = Test.FAIL
+
+    hand_rank, high_card = get_hand_rank(one_pair, [])
+    if(hand_rank != 1 and high_card != 0):
+        print("get_hand_rank_UT fail: one_pair")
+        UT = Test.FAIL
+
+    
+    hand_rank, high_card = get_hand_rank(high_card_list, [])
+    if(hand_rank != 0 and high_card != 0):
+        print("get_hand_rank_UT fail: high_card")
+        UT = Test.FAIL
+
+    if(UT == Test.PASS):
+        print("get_hand_rank_UT PASS")
+    else:
+        print("get_hand_rank_UT FAIL")    
+    
+def is_straight_flush_UT():
+
+    UT = Test.PASS
+
+    # positive cases
+    hand = [Card(0,3), Card(0,4)]
+    public = [Card(0,5), Card(0,6), Card(0,7), Card(2,12), Card(0,3)]
+    straight_flush, high_card = is_straight_flush(hand, public) # sraight flush
+    if(straight_flush != True or high_card != 7):
+        UT = Test.FAIL
+        print("is_straight_flush_UT #1 FAILURE")
+
+    hand = [Card(2,3), Card(2,4)]
+    public = [Card(2,5), Card(2,6), Card(2,7), Card(2,8), Card(2,9)]
+    straight_flush, high_card = is_straight_flush(hand, public)  # all cards make straight flush
+    if(straight_flush != True or high_card != 9):
+        UT = Test.FAIL
+        print("is_straight_flush_UT #2 FAILURE")
+        
+    hand = [Card(2,0), Card(2,1)]
+    public = [Card(2,2), Card(2,3), Card(2,4), Card(1,8), Card(0,12)]
+    straight_flush, high_card = is_straight_flush(hand, public)  # ace low
+    if(straight_flush != True or high_card != 4):
+        UT = Test.FAIL
+        print("is_straight_flush_UT #3 FAILURE")
+
+    hand = [Card(2,0), Card(2,13)]
+    public = [Card(2,12), Card(2,11), Card(2,10), Card(1,8), Card(0,12)]
+    straight_flush, high_card = is_straight_flush(hand, public)  # ace high
+    if(straight_flush != True or high_card != 0):
+        UT = Test.FAIL
+        print("is_straight_flush_UT #4 FAILURE")
+        
+    # negative cases
+    hand = [Card(0,4), Card(1,5)]
+    public = [Card(2,6), Card(2,7), Card(3,8), Card(2,6), Card(0,3)]
+    straight_flush, high_card = is_straight_flush(hand, public) # straight, no flush
+    if(straight_flush != False or high_card != None):
+        UT = Test.FAIL
+        print("is_straight_flush_UT #5 FAILURE")
+
+    hand = [Card(3,0), Card(3,0)]
+    public = [Card(3,2), Card(3,11), Card(3,7), Card(2,7), Card(0,3)]
+    straight_flush, high_card = is_straight_flush(hand, public) # flush, no straight
+    if(straight_flush != False or high_card != None):
+        UT = Test.FAIL
+        print("is_straight_flush_UT #6 FAILURE")
+
+    hand = [Card(0,3), Card(0,2)]
+    public = [Card(1,2), Card(1,11), Card(2,7), Card(2,7), Card(3,3)]
+    straight_flush, high_card = is_straight_flush(hand, public) # no flush or straight
+    if(straight_flush != False or high_card != None):
+        UT = Test.FAIL
+        print("is_straight_flush_UT #7 FAILURE")
+    
+    if(UT == Test.PASS):
+        print("is_straight_flush_UT PASS")
+    else:
+        print("is_straight_flush_UT FAIL")    
+
+def is_four_of_a_kind_UT():
+    UT = Test.PASS
+
+    # positive cases
+    four_of_a_kind, high_card = is_four_of_a_kind(sort_cards([Card(0,3), Card(1,3), Card(2,3), Card(2,12), Card(3,7), Card(2,12), Card(0,3)]))  # four of a kind
+    if(four_of_a_kind != True or high_card != 3):
+        UT = Test.FAIL
+        print("is_four_of_a_kind_UT #1 FAILURE")
+    four_of_a_kind, high_card = is_four_of_a_kind(sort_cards([Card(0,0), Card(1,0), Card(2,0), Card(2,0), Card(3,12), Card(2,12), Card(0,12)])) # 4 of a kind and 3 of a kind
+    if(four_of_a_kind != True or high_card != 0):
+        UT = Test.FAIL
+        print("is_four_of_a_kind_UT #2 FAILURE")
+
+    # negative cases
+    four_of_a_kind, high_card = is_four_of_a_kind(sort_cards([Card(0,4), Card(1,0), Card(2,2), Card(2,12), Card(3,7), Card(2,6), Card(0,3)])) # no matching ranks
+    if(four_of_a_kind != False or high_card != None):
+        UT = Test.FAIL
+        print("is_four_of_a_kind_UT #3 FAILURE")
+    four_of_a_kind, high_card = is_four_of_a_kind(sort_cards([Card(0,0), Card(1,0), Card(2,2), Card(2,0), Card(3,7), Card(2,7), Card(0,3)])) # 3 of a kind
+    if(four_of_a_kind != False or high_card != None):
+        UT = Test.FAIL
+        print("is_four_of_a_kind_UT #4 FAILURE")
+        
+    if(UT == Test.PASS):
+        print("is_four_of_a_kind_UT PASS")
+    else:
+        print("is_four_of_a_kind_UT FAIL")    
+
+def is_full_house_UT():
+    UT = Test.PASS
+
+    # positive cases
+    full_house, high_card = is_full_house(sort_cards([Card(0,0), Card(1,0), Card(2,0), Card(2,12), Card(3,7), Card(2,12), Card(0,3)]))  # full house, ace high
+    if(full_house != True or high_card != 0):
+        UT = Test.FAIL
+        print("is_full_house_UT #1 FAILURE")
+    full_house, high_card = is_full_house(sort_cards([Card(0,0), Card(1,0), Card(2,0), Card(2,0), Card(3,12), Card(2,12), Card(0,12)])) # 4 of a kind and 3 of a kind, ace high
+    if(full_house != True or high_card != 0):
+        UT = Test.FAIL
+        print("is_full_house_UT #2 FAILURE")
+
+    # negative cases
+    full_house, high_card = is_full_house(sort_cards([Card(0,4), Card(1,0), Card(2,2), Card(2,12), Card(3,7), Card(2,6), Card(0,3)])) # no full house
+    if(full_house != False or high_card != None):
+        UT = Test.FAIL
+        print("is_full_house_UT #3 FAILURE")
+    full_house, high_card = is_full_house(sort_cards([Card(0,0), Card(1,0), Card(2,2), Card(2,12), Card(3,7), Card(2,7), Card(0,3)])) # two pair
+    if(full_house != False or high_card != None):
+        UT = Test.FAIL
+        print("is_full_house_UT #4 FAILURE")
+    
+    if(UT== Test.PASS):
+        print("is_full_house_UT PASS")
+    else:
+        print("is_full_house_UT FAIL")    
+
+def is_three_of_a_kind_UT():
+    UT = Test.PASS
+    
+    # positive cases
+    three_of_a_kind, high_card = is_three_of_a_kind(sort_cards([Card(0,3), Card(1,0), Card(2,2), Card(2,3), Card(3,7), Card(2,6), Card(0,3)])) # three of a kind
+    if(three_of_a_kind != True or high_card != 3):
+        UT = Test.FAIL
+        print("is_three_of_a_kind_UT #1 FAILURE")
+    three_of_a_kind, high_card = is_three_of_a_kind(sort_cards([Card(1,3), Card(1,2), Card(1,0), Card(2,3), Card(3,0), Card(2,0), Card(0,3)])) # two three of a kinds, high ace
+    if(three_of_a_kind != True or high_card != 0):
+        UT = Test.FAIL
+        print("is_three_of_a_kind_UT #2 FAILURE")
+        
+    # negative cases
+    three_of_a_kind, high_card = is_three_of_a_kind(sort_cards([Card(0,4), Card(1,0), Card(2,2), Card(2,12), Card(3,7), Card(2,6), Card(0,3)])) # no three of a kind
+    if(three_of_a_kind != False or high_card != None):
+        UT = Test.FAIL
+        print("is_three_of_a_kind_UT #3 FAILURE")
+
+    
+    if(UT == Test.PASS):
+        print("is_three_of_a_kind_UT PASS")
+    else:
+        print("is_three_of_a_kind_UT FAIL")    
+
+def is_pair_UT():
+    UT = Test.PASS
+    
+    # positive cases
+    pair, high_card = is_pair(sort_cards([Card(0,0), Card(1,0), Card(2,2), Card(2,12), Card(3,7), Card(2,6), Card(0,3)])) # one pair
+    if(pair != True or high_card != 0):
+        UT = Test.FAIL
+        print("is_pair_UT #1 FAILURE")
+
+    # negative cases
+    pair, high_card = is_pair(sort_cards([Card(0,4), Card(1,0), Card(2,2), Card(2,12), Card(3,7), Card(2,6), Card(0,3)])) # no pair
+    if(pair != False or high_card != None):
+        UT = Test.FAIL
+        print("is_pair_UT #2 FAILURE")
+        
     if(UT == Test.PASS):
         print("is_pair_UT PASS")
     else:
         print("is_pair_UT FAIL")            
         
 def is_two_pair_UT():
-    pass
+    UT = Test.PASS
+
+    # positive cases
+    two_pair, high_card = is_two_pair(sort_cards([Card(0,0), Card(1,0), Card(2,2), Card(2,12), Card(3,7), Card(2,7), Card(0,3)]))  # two pair, high ace
+    if(two_pair != True or high_card != 0):
+        UT = Test.FAIL
+        print("is_two_pair_UT #1 FAILURE")
+    two_pair, high_card = is_two_pair(sort_cards([Card(0,0), Card(1,0), Card(2,12), Card(2,12), Card(3,7), Card(2,6), Card(0,6)])) # three pair
+    if(two_pair != True or high_card != 0):
+        UT = Test.FAIL
+        print("is_two_pair_UT #2 FAILURE")
+        
+    # negative cases
+    two_pair, high_card = is_two_pair(sort_cards([Card(0,4), Card(1,0), Card(2,2), Card(2,12), Card(3,7), Card(2,6), Card(0,3)])) # no pair
+    if(two_pair != False or high_card != None):
+        UT = Test.FAIL
+        print("is_two_pair_UT #3 FAILURE")
+    two_pair, high_card = is_two_pair(sort_cards([Card(0,0), Card(1,0), Card(2,2), Card(2,12), Card(3,7), Card(2,6), Card(0,3)])) # one pair
+    if(two_pair != False or high_card != None):
+        UT = Test.FAIL
+        print("is_two_pair_UT #4 FAILURE")
+    
+    if(UT == Test.PASS):
+        print("is_two_pair_UT PASS")
+    else:
+        print("is_two_pair_UT FAIL")                
 
 def is_flush_UT():
-    pass
+    UT = Test.PASS
+
+    # positive cases
+
+    # high card ace in hand
+    hand = [Card(0,0), Card(0,2)]
+    public = [Card(0,4), Card(0,3), Card(0,2), Card(0,2), Card(0,1)]
+    
+    flush, high_card = is_flush(hand, public) # 7 cards of same suit
+    if(flush != True or high_card != 0):
+        UT = Test.FAIL
+        print("is_flush_UT #1 FAILURE")
+
+    # high card not in hand, but should return a high card of 3 as that's the highest in hand
+    hand = [Card(0,3), Card(0,2)]
+    public = [Card(3,0), Card(2,0), Card(0,4), Card(0,2), Card(0,0)]
+    flush, high_card = is_flush(hand, public)  # 5 cards of same suit
+    if(flush != True or high_card != 3):
+        UT = Test.FAIL
+        print("is_flush_UT #2 FAILURE")
+
+    
+    # negative cases
+    hand = [Card(0,4), Card(1,0)]
+    public = [Card(2,2), Card(0,12), Card(0,7), Card(2,6), Card(2,3)]    
+    flush, high_card = is_flush(hand, public) # 3 cards of same suit
+    if(flush != False or high_card != None):
+        UT = Test.FAIL
+        print("is_flush_UT #3 FAILURE")
+        
+    hand = [Card(3,4), Card(3,0)]
+    public = [Card(2,2), Card(1,12), Card(0,7)]
+    flush, high_card = is_flush(hand, public) # 2 cards of same suit
+    if(flush != False or high_card != None):
+        UT = Test.FAIL
+        print("is_flush_UT #4 FAILURE")
+        
+    hand = [Card(0,4), Card(1,0)]
+    public = [Card(2,2), Card(3,12)]    
+    flush, high_card = is_flush(hand, public) # 0 cards of same suit
+    if(flush != False or high_card != None):
+        UT = Test.FAIL
+        print("is_flush_UT #5 FAILURE")
+        
+    hand = [Card(0,0), Card(1,0)]
+    public = [Card(2,4), Card(0,3), Card(0,2), Card(2,2), Card(0,1)]
+    flush, high_card = is_flush(hand, public)  # 4 cards of same suit
+    if(flush != False or high_card != None):
+        UT = Test.FAIL
+        print("is_flush_UT #6 FAILURE")
+
+    if(UT == Test.PASS):
+        print("is_flush_UT PASS")
+    else:
+        print("is_flush_UT FAIL")            
 
 def is_straight_UT():
-    pass
+    UT = Test.PASS
+
+    # positive cases
+    straight, high_card = is_straight(sort_cards([Card(0,0), Card(1,0), Card(2,4), Card(2,3), Card(3,2), Card(2,2), Card(0,1)]))   # straight with some duplicates, low card ace
+    if(straight != True or high_card != 4):
+        UT = Test.FAIL
+        print("is_straight_UT #1 FAILURE")
+
+    straight, high_card = is_straight(sort_cards([Card(0,12), Card(1,0), Card(2,4), Card(2,3), Card(3,11), Card(2,2), Card(0,1)])) # straight with no duplicates, low card ace
+    if(straight != True or high_card != 4):
+        UT = Test.FAIL
+        print("is_straight_UT #2 FAILURE")
+
+    straight, high_card = is_straight(sort_cards([Card(0,0), Card(1,13), Card(2,12), Card(2,11), Card(3,10), Card(2,2), Card(0,1)])) # straight with high card ace
+    if(straight != True or high_card != 0):
+        UT = Test.FAIL
+        print("is_straight_UT #3 FAILURE")
+        
+    # negative cases
+    straight, high_card = is_straight(sort_cards([Card(0,4), Card(1,0), Card(2,2), Card(2,12), Card(3,7), Card(2,6), Card(0,3)])) # 3 cards to a straight
+    if(straight != False or high_card != None):
+        UT = Test.FAIL
+        print("is_straight_UT #4 FAILURE")
+
+    straight, high_card = is_straight(sort_cards([Card(0,4), Card(1,1), Card(2,2), Card(2,12), Card(3,7), Card(2,6), Card(0,3)])) # 4 cards to a straight
+    if(straight != False or high_card != None):
+        UT = Test.FAIL
+        print("is_straight_UT #5 FAILURE")
 
 
+    if(UT == Test.PASS):
+        print("is_straight_UT PASS")
+    else:
+        print("is_straight_UT FAIL")            
+        
 def deck_UT():
     UT_func_list = [card_class_UT, deck_class_UT, sort_cards_UT, get_hand_rank_UT, is_straight_flush_UT, is_four_of_a_kind_UT, is_full_house_UT, is_three_of_a_kind_UT, is_pair_UT, is_two_pair_UT, is_flush_UT, is_straight_UT]
 
     for function in UT_func_list:
         function()
-
+        
 if(__name__ == "__main__"):
     deck_UT()
